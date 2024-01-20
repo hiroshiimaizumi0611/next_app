902d7ff9ca2a6e403bc3148bedfe6802d775aeca
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-01-20 21:57:38 +0900

    prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      { name: "Alice", email: "alice@example.com" },
+      { name: "Bob", email: "bob@example.com" },
+      { name: "Charlie", email: "charlie@example.com" },
+      { name: "Dana", email: "dana@example.com" },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
